12313ac6021f240ddeef1ac12b1bc17697d6e082
Aniket Singh <aniketsingh2151@gmail.com>
2025-03-31 21:02:09 +0530

    resolved all type errors, common for zod and backend-common for JWT done

---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
+
+export const middleware = (req:Request, res: Response, next: NextFunction) => {
+    const token = req.headers["authorization"] ?? "";
+
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    //@ts-ignore
+    if(decoded.userId){
+        //@ts-ignore
+        req.userId = decoded.userId; 
+    } else {
+        res.status(403).json({
+            message: "Unauthorized"
+        })
+    }
+}
